6572c2683dc26e251a7c277631aeec1b2ddf82df
Rafael_Cepeda <rcepeda1@yahoo.com>
2024-05-23 21:17:12 -0600

    Primera carga de todos los archivos

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document is ready");
+    // Aquí puedes agregar cualquier funcionalidad de JavaScript adicional que necesites
+});
